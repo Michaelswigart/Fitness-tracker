1234a59c692b745df4131458452450c69c2e0081
michael swigat <sherlockholmes.swigart92@gmail.com>
2021-04-11 19:40:47 -0500

    fixed routs

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
-// if /exercise, return exercise.html
-
+// if /exercise, return exercise
+const router = require('express').Router();
 const path = require("path");
-module.exports = function (app) {
-  app.get("/exercise", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
-  });
-};
+router.get('/exercise',(req, res)=>{
+  res.sendFile(path.join(__dirname,'../public/exercise.html'))
+})
+
+router.get('/stats',(req, res)=>{
+  res.sendFile(path.join(__dirname,'../public/stats.html'))
+})
+router.get('/',(req, res)=>{
+  res.sendFile(path.join(__dirname,'../public/index.html'))
+})
+
+module.exports = router;
