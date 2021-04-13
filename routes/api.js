b4fc5e1af158c9620d4a07a62171893a9cd302b7
michael swigat <sherlockholmes.swigart92@gmail.com>
2021-04-12 22:04:08 -0500

    safafd

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,8 @@ let { Workout } = require('../models/');
 const router = require('express').Router();
  
     // GET route /api/workouts
-    router.get('/api/workouts', function(req, res) {
+    router.get('/api/workouts',(req, res) => {
+     console.log("router.get")
         // Returning the current list of workouts
         Workout.find({})
         .then((data)=> {
@@ -22,19 +23,22 @@ const router = require('express').Router();
 
     // PUT route /api/workouts
 router.put('/api/workouts/:id'), (req,res) => {
-    console.log("putrout called")
-   Workout.findOneAndUpdate({
-       _id: req.params.id
-   },{
-       $inc:{totalDuration:req.body.duration}, $push:{exercises:req.body}
-   },{
-       new:true
-   }).then(data =>{
-       console.log("putrout succes")
-      res.json(data)
-   }).catch((err) => {
-       res.json(err);
-   })
+//     console.log("putrout called")
+//    Workout.findOneAndUpdate({
+//        _id: req.params.id
+//    },{
+//        $inc:{totalDuration:req.body.duration}, $push:{exercises:req.body}
+//    },{
+//        new:true
+//    }).then(data =>{
+//        console.log("putrout succes")
+//       res.json(data)
+//    }).catch((err) => {
+//        console.log(err);
+//        res.json(err);
+//    })
+console.log("hello");
+res.send("hello");
 };
 
 
